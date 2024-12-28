3fa2b1b744c781f1546cd8217ad1106d460f9595
Arty <arty@arty.com>
2024-12-28 03:28:40 -0500

    add string rule tests

---
+++ StringTests.cs
@@ -0,0 +1,24 @@
+﻿using Ferret;
+
+namespace FerretTesting
+{
+    public class StringTests
+    {
+        [Test]
+        public void StringRemove()
+        {
+            var data = "string myString = \"Hello World!\";";
+            var tokens = new Tokenizer(data).Parse();
+            var stream = new TokenStream(tokens);
+
+            new TokenLexer(stream)
+                .AppendRule(new StringRule())
+                .ProcessRules();
+
+            string expected = "string myString = ;";
+            string actual = stream.ToString()!;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
